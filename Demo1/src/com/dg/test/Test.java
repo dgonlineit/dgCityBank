a2eb318fbd952a819a4cd900747e328dc0a840b3
TL <dve2@gmail.com>
2020-04-10 12:33:55 +0530

    "Adding New File for testing"

---
+++ Demo1/src/com/dg/test/Test.java
@@ -0,0 +1,13 @@
+package com.dg.test;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Welcome To Selnium...........!");
+
+	}
+
+}
